224d4669188203ba93e4715aae96f35a89c1d8a7
Robson Alves <111312704+robsonalvesdev@users.noreply.github.com>
2023-11-22 09:13:01 -0300

    feat: factory dbcontext

---
+++ src/DevIO.Data/Context/MeuDbContextDesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContextDesignTimeDbContextFactory : IDesignTimeDbContextFactory<MeuDbContext>
+    {
+        public MeuDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<MeuDbContext>();
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            builder
+                .UseSqlServer(configuration.GetConnectionString("SqlServer"));
+
+            return new MeuDbContext(builder.Options);
+        }
+    }
+}
