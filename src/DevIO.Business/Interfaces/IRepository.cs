9767de6a2a51f1227310087d1dfa2dcabe94b00a
Robson Alves <111312704+robsonalvesdev@users.noreply.github.com>
2023-11-21 22:59:02 -0300

    feat: Implementação repository

--- src/DevIO.Business/Interfaces/IRepository.cs
+++ src/DevIO.Business/Interfaces/IRepository.cs
@@ -5,12 +5,12 @@ namespace DevIO.Business.Interfaces
 {
     public interface IRepository<TEntity> : IDisposable where TEntity : Entity
     {
-        Task AdicionarAsync(TEntity entity);
         Task<TEntity> ObterPorIdAsync(Guid id);
         Task<List<TEntity>> ObterTodos();
-        Task AtualizarAsync(TEntity entity);
-        Task RemoverAsync(Guid id);
         Task<IEnumerable<TEntity>> BuscarAsync(Expression<Func<TEntity, bool>> predicate);
+        Task AdicionarAsync(TEntity entity);
+        Task AtualizarAsync(TEntity entity);        
+        Task RemoverAsync(Guid id);        
         Task<int> SaveChangesAsync();
     }
 }
---
+++ src/DevIO.Data/Repository/Repository.cs
@@ -0,0 +1,63 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace DevIO.Data.Repository
+{
+    public abstract class Repository<TEntiry> : IRepository<TEntiry> where TEntiry : Entity, new()
+    {
+        protected readonly MeuDbContext Db;
+        protected readonly DbSet<TEntiry> DbSet;
+
+        protected Repository(MeuDbContext db)
+        {
+            Db = db;
+            DbSet = db.Set<TEntiry>();
+        }
+
+        public virtual async Task<TEntiry?> ObterPorIdAsync(Guid id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public virtual async Task<List<TEntiry>> ObterTodos()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public virtual async Task<IEnumerable<TEntiry>> BuscarAsync(Expression<Func<TEntiry, bool>> predicate)
+        {
+            return await DbSet.AsNoTracking().Where(predicate).ToListAsync();
+        }
+
+        public virtual async Task AdicionarAsync(TEntiry entity)
+        {
+            await DbSet.AddAsync(entity);
+            await SaveChangesAsync();
+        }
+
+        public virtual async Task AtualizarAsync(TEntiry entity)
+        {
+            DbSet.Update(entity);
+            await SaveChangesAsync();
+        }
+
+        public virtual async Task RemoverAsync(Guid id)
+        {
+            DbSet.Remove(new TEntiry { Id = id });
+            await SaveChangesAsync();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await Db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Db.Dispose();
+        }
+    }
+}
