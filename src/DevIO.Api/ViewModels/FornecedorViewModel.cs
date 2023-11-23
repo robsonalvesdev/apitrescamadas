d5deec10ec83ca235b421709dbdf160c0e3c1c02
Robson Alves <111312704+robsonalvesdev@users.noreply.github.com>
2023-11-23 16:39:55 -0300

    feat: novas anotações

--- src/DevIO.Api/ViewModels/FornecedorViewModel.cs
+++ src/DevIO.Api/ViewModels/FornecedorViewModel.cs
@@ -8,12 +8,15 @@ namespace DevIO.Api.ViewModels
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        [DeniedValues("none", "lol")]
         public string? Nome { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
+        [DeniedValues("none", "lol")]
         public string? Documento { get; set; }
 
+        [AllowedValues("1", "2")]
         public int TipoFornecedor { get; set; }
 
         public bool Ativo { get; set; }
